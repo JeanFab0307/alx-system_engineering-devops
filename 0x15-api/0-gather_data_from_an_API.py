751a53d0a24f335015ec54e80d499de38a455cd5
JeanFab0307 <jfrandriatsimivony@gamil.com>
2023-10-23 18:27:58 +0300

    Insure code is not executed when imported

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,25 +6,25 @@ import json
 import requests
 from sys import argv
 
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com{}'
+    path = '/users/{}'.format(str(argv[1]))
+    response = requests.get(url.format(path))
+    employee = response.json()
 
-url = 'https://jsonplaceholder.typicode.com{}'
-path = '/users/{}'.format(str(argv[1]))
-response = requests.get(url.format(path))
-employee = response.json()
+    path = '/todos/?userId={}'.format(str(argv[1]))
+    response = requests.get(url.format(path))
+    tasks = response.json()
 
-path = '/todos/?userId={}'.format(str(argv[1]))
-response = requests.get(url.format(path))
-tasks = response.json()
+    taskNumber = 0
+    taskCompleted = 0
+    for task in tasks:
+        if task['completed'] is True:
+            taskCompleted += 1
+            taskNumber += 1
 
-taskNumber = 0
-taskCompleted = 0
-for task in tasks:
-    if task['completed'] is True:
-        taskCompleted += 1
-    taskNumber += 1
-
-print('Employee {} is done with tasks({}/{}):'.
-      format(employee['name'], taskCompleted, taskNumber))
-for task in tasks:
-    if task['completed'] is True:
-        print('\t ' + task['title'])
+            print('Employee {} is done with tasks({}/{}):'.
+                  format(employee['name'], taskCompleted, taskNumber))
+            for task in tasks:
+                if task['completed'] is True:
+                    print('\t ' + task['title'])

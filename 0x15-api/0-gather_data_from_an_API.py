99423ac9eacc853dc9385e6693696ff80460c76d
JeanFab0307 <jfrandriatsimivony@gamil.com>
2023-10-23 18:22:31 +0300

    Add task0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Uses a specific RESTAPI to return values in a determined format
+"""
+import json
+import requests
+from sys import argv
+
+
+url = 'https://jsonplaceholder.typicode.com{}'
+path = '/users/{}'.format(str(argv[1]))
+response = requests.get(url.format(path))
+employee = response.json()
+
+path = '/todos/?userId={}'.format(str(argv[1]))
+response = requests.get(url.format(path))
+tasks = response.json()
+
+taskNumber = 0
+taskCompleted = 0
+for task in tasks:
+    if task['completed'] is True:
+        taskCompleted += 1
+    taskNumber += 1
+
+print('Employee {} is done with tasks({}/{}):'.
+      format(employee['name'], taskCompleted, taskNumber))
+for task in tasks:
+    if task['completed'] is True:
+        print('\t ' + task['title'])
